fdee8ed7807318f593a1289e55bb15260b645a07
Luis F Rodriguez <luis@fgshome.com>
2016-01-23 12:02:09 -0500

    Implementation of TimeStamp on a comment.

--- angular/app/scripts/directives/commentlist/commentlist.js
+++ angular/app/scripts/directives/commentlist/commentlist.js
@@ -6,19 +6,59 @@
  * @description
  * # commentList
  */
+var milliPer = {
+        SEC:    1000,
+        MI:     60000,
+        HR:     3600000,
+        DAY:    86400000
+};
+
 angular.module('commentList', ['comment'])
   .directive('commentList', function () {
     return {
       template: '<div class="commentList">' +
-                  '<comment-model ng-repeat="comment in comments" author="{{comment.author}}">' +
-                    '{{comment.msg}}' +
-                  '</comment-model>' +
-                  '<span ng-if="comments.length < 1">No comments yet</span>' +
+                    '<table class="table table-bordered">' +
+                    '<thead><tr>' +
+                        '<th>Author</th><th>Comment</th><th>Posted</th>' +
+                    '</tr></thead>' +
+                    '<tbody><tr ng-repeat="comment in comments">' +
+                    '<td>{{comment.author}}</td><td>{{comment.msg}}</td><td>{{comment.timestamp | ts}}</td>' +
+                    '</tr></tbody>' +
+                    '</table>' +
                 '</div>',
       restrict: 'E',
       scope: {
         comments: '='
       },
+
       link: function postLink(scope, element, attrs) {}
     };
-  });
+  }).filter('ts', function($filter) { 
+    return function(ts) {
+        var retVal      = 'no date',
+            timeStamp   = parseInt(ts, 10);
+        
+        if (!isNaN(timeStamp)) {
+            var delta = new Date().getTime() - timeStamp,
+                        qualifier;
+            
+            if (delta > milliPer.DAY - milliPer.SEC) {
+                retVal = Math.floor(delta/milliPer.DAY);
+                qualifier = 'day';
+            } else if (delta > milliPer.HR - 1000) {
+                retVal = Math.floor(delta/milliPer.HR);
+                qualifier = 'hour';
+            } else {
+                retVal = Math.floor(delta/milliPer.MI);
+                if (retVal <= 0) {
+                    retVal = 1;
+                }
+                qualifier = 'minute';
+            }
+            retVal += ' ' + qualifier + (retVal > 1 ? 's' : '') + ' ago';
+        }
+        return retVal;
+    }
+    });
+
+
